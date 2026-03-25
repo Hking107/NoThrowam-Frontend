820718a759c16d7fbfeade933ea18a9a69ae0b5d
JeffreyYAJ <jeffreyyaj@gmail.com>
2026-03-25 21:02:07 +0100

    modified home and landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,22 +8,15 @@ import { CtaSection } from "../components/CtaSection";
 export function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col w-full selection:bg-brand-green/20 selection:text-brand-text">
-      {/* 1. Header & Navigation */}
       {/* <Navbar /> */}
 
-      {/* Main Content Area */}
       <main className="grow">
-        {/* 2. Hero Section (Guest Flow) */}
         <Hero />
 
-        {/* 3. Promotional & Advertisement Section */}
         <SuccessStories />
-
-        {/* 4. Actor Entry Points (Call to Action) */}
         <ActorCards />
       </main>
 
-      {/* Standard Footer */}
       <CtaSection />
       <Footer />
     </div>
