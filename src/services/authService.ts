e4de72b955b6607babaed386e5e6c955083cc5d6
JeffreyYAJ <jeffreyyaj@gmail.com>
2026-04-08 15:05:37 +0100

    commit

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:8000";
+const API_BASE = import.meta.env.VITE_API_BASE || "http://172.17.0.1:8000";
 
 const headers = {
   "Content-Type": "application/json",
