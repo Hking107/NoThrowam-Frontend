4f634275a865336fcdd5307af3c41beea1d613aa
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-15 11:29:56 +0100

    feat: implement waste report submission API integration and fix proxy configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
+  server: {
     proxy: {
-      '/api': {
-        target: 'http:172.17.0.1:8000',
+      "/api": {
+        target: "http://172.17.0.1:8000",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-
+      },
+    },
+  },
 });
