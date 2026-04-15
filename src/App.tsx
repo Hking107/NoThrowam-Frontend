88f9c922939a8fb66f59c7737535c19089e56eea
kyler004 <lowelrayannkenneyemtsa@gmail.com>
2026-04-15 13:28:00 +0100

    feat: implement global smooth scrolling using Lenis and GSAP integration

--- src/App.tsx
+++ src/App.tsx
@@ -16,58 +16,61 @@ import SellerDashboard from "./Seller_Section/SellerDashboard";
 import { ReportWaste } from "./pages/ReportWaste";
 import { NotFound } from "./pages/NotFound";
 import { Unauthorized } from "./pages/Unauthorized";
+import { SmoothScroll } from "./components/SmoothScroll";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignIn />} />
+      <SmoothScroll>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SignIn />} />
 
-        {/* Must be placed in a protected Route later on */}
-        <Route path="/report-waste" element={<ReportWaste />} />
+          {/* Must be placed in a protected Route later on */}
+          <Route path="/report-waste" element={<ReportWaste />} />
 
-        {/* Signup Routes */}
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signup/seller" element={<SellerSignup />} />
-        <Route path="/signup/customer" element={<CustomerSignup />} />
-        <Route path="/signup/manager" element={<ManagerSignup />} />
+          {/* Signup Routes */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup/seller" element={<SellerSignup />} />
+          <Route path="/signup/customer" element={<CustomerSignup />} />
+          <Route path="/signup/manager" element={<ManagerSignup />} />
 
-        {/* OTP Verification */}
-        <Route path="/verify-otp" element={<VerifyOTP />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        
-        {/* Protected Dashboard Routes */}
-        <Route
-          path="/dashboard_customer"
-          element={
-            <ProtectedRoute requiredRole="CUSTOMER">
-              <CustomerMain />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/manager"
-          element={
-            <ProtectedRoute requiredRole="MANAGER">
-              <ManagerMain />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard_seller"
-          element={
-            <ProtectedRoute requiredRole="SELLER">
-              <SellerDashboard />
-            </ProtectedRoute>
-          }
-        />
+          {/* OTP Verification */}
+          <Route path="/verify-otp" element={<VerifyOTP />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          
+          {/* Protected Dashboard Routes */}
+          <Route
+            path="/dashboard_customer"
+            element={
+              <ProtectedRoute requiredRole="CUSTOMER">
+                <CustomerMain />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/manager"
+            element={
+              <ProtectedRoute requiredRole="MANAGER">
+                <ManagerMain />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard_seller"
+            element={
+              <ProtectedRoute requiredRole="SELLER">
+                <SellerDashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Error Pages */}
-        <Route path="/unauthorized" element={<Unauthorized />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          {/* Error Pages */}
+          <Route path="/unauthorized" element={<Unauthorized />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </SmoothScroll>
     </Router>
   );
 }
